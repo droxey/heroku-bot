0f28bc9e5322ad4b602aa1ff27d4fd26a4b8a771
Dani Roxberry <dani@bitoriented.com>
2018-04-07 03:04:34 -0700

    [fix] test

--- ChatterBot.py
+++ ChatterBot.py
@@ -7,15 +7,19 @@ import time
 from dotenv import load_dotenv
 load_dotenv()
 
-consumer_key = os.getenv("CONSUMER_KEY")
+consumer_key = os.getenv("CONSUMER_KEY") or os.environ.get(
+    "CONSUMER_KEY", "bad")
 consumer_secret = os.getenv("CONSUMER_SECRET")
 access_token = os.getenv("ACCESS_TOKEN")
 access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
-# Setup Tweepy API Authentication
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+try:
+    # Setup Tweepy API Authentication
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+except Exception, e:
+    print(e)
 
 
 # Create a function that tweets
